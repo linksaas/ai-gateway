d5ec4aaada66a6219f7fcd1b28ae14cf66de4f7d
panleiming <panleiming@linksaas.pro>
2023-03-12 11:22:57 +0800

    adjust example code

--- example/coding_provider_codegeex.go
+++ example/coding_provider_codegeex.go
@@ -22,7 +22,10 @@ type CommonResponse struct {
 	Result  CommonResponseResult `json:"result"`
 }
 
-func runComplete(lang, content string) ([]string, error) {
+// process /api/coding/complete/:lang
+func Complete(lang, content string) []string {
+	lang = strings.ToUpper(lang)[:1] + lang[1:]
+
 	payload := map[string]interface{}{
 		"n":      1,
 		"lang":   lang,
@@ -30,78 +33,36 @@ func runComplete(lang, content string) ([]string, error) {
 	}
 	payloadData, err := json.Marshal(payload)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return nil
 	}
 	req, err := http.NewRequest("POST", "https://wudao.aminer.cn/os/api/api/v2/multilingual_code/generate", bytes.NewBuffer(payloadData))
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return nil
 	}
 	req.Header.Add("Origin", "https://codegeex.cn")
 	req.Header.Add("Referer", "https://codegeex.cn/")
 	req.Header.Add("Content-Type", "application/json;charset=UTF-8")
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return nil
 	}
 	defer res.Body.Close()
 	resData, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return nil
 	}
 	response := &CommonResponse{}
 	err = json.Unmarshal(resData, response)
 	if err != nil {
-		return nil, err
-	}
-	if response.Result.Output.Code == nil {
-		return nil, fmt.Errorf("error response")
-	}
-	return response.Result.Output.Code, nil
-}
-
-// process /api/coding/complete/:lang
-func Complete(lang, content string) []string {
-	lang = strings.ToUpper(lang)[:1] + lang[1:]
-	lines := strings.Split(content, "\n")
-	firstLine := ""
-	for _, line := range lines {
-		line = strings.Trim(line, " \r\n\t")
-		if line != "" {
-			firstLine = line
-			break
-		}
-	}
-	if firstLine == "" {
+		fmt.Println(err)
 		return nil
 	}
 
-	allResult := []string{}
-	stepCount := 0
-	for {
-		stepCount++
-		if stepCount > 100 {
-			break
-		}
-		lines, err := runComplete(lang, content)
-		if err != nil {
-			fmt.Println(err)
-			return nil
-		}
-		if len(lines) == 0 {
-			break
-		}
-		// fmt.Println(lines[0])
-		content += "\n"
-		content += lines[0]
-		allResult = append(allResult, lines[0])
-		if strings.Contains(lines[0], firstLine) {
-			break
-		}
-		if strings.Trim(lines[0], " \r\n\t") == "" {
-			break
-		}
-	}
-	return []string{strings.Join(allResult, "")}
+	return response.Result.Output.Code
 }
 
 // process /api/coding/convert/:lang
